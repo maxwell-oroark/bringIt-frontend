06e8a916a25145e09c000c0809a2e9d87c18bc70
Maxwell O'Roark <m.d.oroark@gmail.com>
2016-04-05 11:27:23 -0600

    added progress bar!

--- static/javascripts/controller.js
+++ static/javascripts/controller.js
@@ -5,20 +5,32 @@ angular.module('app')
     $scope.survey = surveyBuilder.survey;
     console.log($scope.survey)
     $scope.currentQ = 0;
-
-    console.log($scope.survey[0].answers)
-
     $scope.symbols = []
-    $scope.meter = []
+    $scope.percentage = 0
 
 
 
     $scope.click = function($index){
+      move();
       $scope.currentQ++;
       $scope.symbols.push($index)
-      $scope.meter.push($index)
+      $scope.percentage += 20;
       console.log($scope.symbols);
     }
+
+    function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
 
     // var glyphiconFinder = function($index){
     //   var glyph = {};
--- static/javascripts/factory.js
+++ static/javascripts/factory.js
@@ -6,26 +6,22 @@ angular.module('app')
     // need to figure out how to have dynamic input types
 
     // QUESTION ONE
-    new question("Where do you want to bring it?", "radio", ["city", "mountain", "island"])
+    new question("Where do you want to bring it?", ["city", "rural", "mountains","other"])
       //QUESTION TWO
-    new question("When do you want to bring it and for how long?", "radio", [])
+    new question("When do you want to bring it and for how long?", ["1 day", "3-5 days", "7+ days"])
         //QUESTION THREE
-    new question("How many do you think you can bring?")
+    new question("How many do you think you can bring?", ["50-100","100-300","300-700", "1000+"])
             //QUESTION FOUR
-    new question("I want to be near?: ")
+    new question("I want to be near?", ["transportation","recreation","arts & music"])
                 //QUESTION FIVE
-    new question("What do you want to bring? (Music Sports Health/Fitness Information, etc.)")
+    new question("What do you want to bring?",["information", "fitness","music"])
                     //QUESTION SIX
-    new question("Would you like information about: lodging, restaurants, catering?")
+    new question("Would you like information about lodging, restaurants, and catering?", ["yes","no"])
 
 
-
-
-
-    function question(content, datatype, answers){
+    function question(content, answers){
 
       this.content = content;
-      this.datatype = datatype;
       this.answers = answers;
       survey.push(this)
 
